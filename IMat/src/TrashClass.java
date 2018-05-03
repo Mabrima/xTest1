5300ad66b749fa478b9e0f95c059f1243ad364ac
Viktoria Enderstein <viktoriaenderstein@Viktorias-MacBook-Pro.local>
2018-05-03 10:46:30 +0200

    vickan testar

--- IMat/src/TrashClass.java
+++ IMat/src/TrashClass.java
@@ -2,5 +2,7 @@ public class TrashClass {
     public static void main(String[] args) {
         System.out.print(System.getProperty("user.home"));
         // I can edit
+
+        //viktoria skriver
     }
 }
