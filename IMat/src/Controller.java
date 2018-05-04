5d668c54d12aa2bf08d1c9a6b50fde1dd426b5da
Robin Arkblad <robinkarlola.arkblad@gmail.com>
2018-05-04 11:04:31 +0200

    saving a few new ideas

--- IMat/src/Controller.java
+++ IMat/src/Controller.java
@@ -1,14 +1,38 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import se.chalmers.cse.dat216.project.*;
 
+
+
 public class Controller {
+    @FXML
+    private Button testButton;
+    private TextField testText;
 
 
     //För att demonstrera hur dataHandlern kommer fungera
     public void test(){
         IMatDataHandler dataHandler = IMatDataHandler.getInstance();
         dataHandler.findProducts("type");
+        Customer customer = dataHandler.getCustomer();
+        CreditCard creditCard = dataHandler.getCreditCard();
+
+
 
     }
 
+    public void initialize(){
+
+
+        testButton.defaultButtonProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+            }
+        });
+    }
 
 }
---
+++ IMat/src/CustomList.java
@@ -0,0 +1,34 @@
+import se.chalmers.cse.dat216.project.*;
+
+import java.util.Date;
+import java.util.List;
+
+public class CustomList {
+    List<ShoppingItem> items;
+    Date date;
+
+    public CustomList(Date date, List<ShoppingItem> items){
+        this.date = date;
+        this.items = items;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<ShoppingItem> getItems() {
+        return this.items;
+    }
+
+    public void setItems(List<ShoppingItem> items) {
+        this.items = items;
+    }
+
+
+
+
+}
