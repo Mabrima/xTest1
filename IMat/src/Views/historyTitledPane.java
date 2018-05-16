bfea5eafd451182b5bb064c0297dfbf2a0f1470d
Alice Olsson <alice_olsson@hotmail.com>
2018-05-16 19:17:44 +0200

    två klasser och fxml History

---
+++ IMat/src/Views/historyTitledPane.java
@@ -0,0 +1,20 @@
+package Views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+
+public class historyTitledPane extends TitledPane {
+
+    @FXML
+    private TitledPane historyTitledPane;
+    @FXML
+    private AnchorPane historyTitledPaneAnchorPane;
+    @FXML
+    private Button historyTitledPaneRemoveButton, historyTitledPaneLaggVarukorgButton;
+    @FXML
+    private Label historyTitledPanePrice;
+
+}
